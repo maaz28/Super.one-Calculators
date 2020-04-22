a190530758b20ae8a59ba7d1bf8163e4d29b1f4f
Noor Muhammad <connectwithnoor1@gmail.com>
2020-04-22 14:44:27 +0500

    styled: range sliders

--- src/components/common/SliderLevel.js
+++ src/components/common/SliderLevel.js
@@ -1,23 +1,23 @@
-import React from "react";
-import Slider from "@material-ui/core/Slider";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import Slider from '@material-ui/core/Slider';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
-    color: "#3880ff",
+    color: '#3880ff',
     height: 2,
-    padding: "15px 0",
+    padding: '15px 0',
   },
   thumb: {
     height: 28,
     width: 28,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     // boxShadow: iOSBoxShadow,
     marginTop: -14,
     marginLeft: -14,
-    "&:focus, &:hover, &$active": {
+    '&:focus, &:hover, &$active': {
       boxShadow:
-        "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
+        '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
       // Reset on touch devices, it doesn't add specificity
       // "@media (hover: none)": {
       //   boxShadow: iOSBoxShadow,
@@ -26,7 +26,7 @@ const useStyles = makeStyles({
   },
   active: {},
   markLabel: {
-    marginTop: 15,
+    marginTop: 33,
   },
   track: {
     height: 2,
@@ -34,151 +34,127 @@ const useStyles = makeStyles({
   rail: {
     height: 2,
     opacity: 0.5,
-    backgroundColor: "#bfbfbf",
+    backgroundColor: '#bfbfbf',
   },
   mark: {
-    backgroundColor: "#bfbfbf",
+    backgroundColor: '#bfbfbf',
     height: 8,
     width: 1,
-    marginTop: 10,
+    marginTop: 28,
   },
 });
 const levels = [
   {
     value: 1,
-    label: "1",
+    label: '1',
   },
   {
     value: 2,
-    label: "2",
+    label: '2',
   },
   {
     value: 3,
-    label: "3",
+    label: '3',
   },
   {
     value: 4,
-    label: "4",
+    label: '4',
   },
   {
     value: 5,
-    label: "5",
+    label: '5',
   },
   {
     value: 6,
-    label: "6",
+    label: '6',
   },
   {
     value: 7,
-    label: "7",
+    label: '7',
   },
   {
     value: 8,
-    label: "8",
+    label: '8',
   },
   {
     value: 9,
-    label: "9",
+    label: '9',
   },
   {
     value: 10,
-    label: "10",
+    label: '10',
   },
   {
     value: 11,
-    label: "11",
+    label: '11',
   },
   {
     value: 12,
-    label: "12",
+    label: '12',
   },
   {
     value: 13,
-    label: "13",
+    label: '13',
   },
   {
     value: 14,
-    label: "14",
+    label: '14',
   },
   {
     value: 15,
-    label: "15",
+    label: '15',
   },
 ];
 
-const AirbnbSlider = withStyles({
+const RangeSlider = withStyles({
+  root: {
+    color: '#AF00CC',
+    height: 120,
+  },
   thumb: {
-    height: 27,
-    width: 27,
-    backgroundColor: "#fff",
-    border: "1px solid currentColor",
-    marginTop: -12,
-    marginLeft: -13,
-    boxShadow: "#ebebeb 0px 2px 2px",
-    "&:focus, &:hover, &$active": {
-      boxShadow: "#ccc 0px 2px 3px 1px",
+    width: 40,
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    border: '1px solid #AF00CC',
+    marginTop: -17,
+    marginLeft: -18,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
     },
-    "& .bar": {
+    '& .bar': {
       // display: inline-block !important;
-      height: 9,
+      height: 15,
       width: 1,
-      backgroundColor: "currentColor",
-      marginLeft: 1,
-      marginRight: 1,
+      backgroundColor: '#AF00CC',
+      marginLeft: 2,
+      marginRight: 2,
     },
   },
-});
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
 
 function valueLabelFormat(value) {
   return levels.findIndex((mark) => mark.value === value) + 1;
 }
 
-function AirbnbThumbComponent(props) {
+function RangeSliderThumbComponent(props) {
   return (
-    <span
-      style={{
-        height: 27,
-        width: 27,
-        backgroundColor: "#fff",
-        border: "1px solid currentColor",
-        marginTop: -12,
-        marginLeft: -13,
-        boxShadow: "#ebebeb 0px 2px 2px",
-        "&:focus, &:hover, &$active": {
-          boxShadow: "#ccc 0px 2px 3px 1px",
-        },
-      }}
-    >
-      <span
-        className="bar"
-        style={{
-          height: 9,
-          width: 1,
-          backgroundColor: "currentColor",
-          marginLeft: 1,
-          marginRight: 1,
-        }}
-      />
-      <span
-        className="bar"
-        style={{
-          height: 9,
-          width: 1,
-          backgroundColor: "currentColor",
-          marginLeft: 1,
-          marginRight: 1,
-        }}
-      />
-      <span
-        className="bar"
-        style={{
-          height: 9,
-          width: 1,
-          backgroundColor: "currentColor",
-          marginLeft: 1,
-          marginRight: 1,
-        }}
-      />
+    <span {...props}>
+      <span className="bar" />
+      <span className="bar" />
+      <span className="bar" />
     </span>
   );
 }
@@ -188,11 +164,8 @@ export default function SliderLevel({ valueChange }) {
 
   return (
     <div className={classes.root}>
-      <Slider
-        style={{
-          color: "#b267cc",
-        }}
-        // ThumbComponent={AirbnbThumbComponent}
+      <RangeSlider
+        ThumbComponent={RangeSliderThumbComponent}
         defaultValue={7}
         valueLabelFormat={valueLabelFormat}
         onChange={(ev, value) => valueChange(value)}
--- src/components/common/SliderReferral.js
+++ src/components/common/SliderReferral.js
@@ -1,24 +1,23 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Slider from "@material-ui/core/Slider";
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
 
 const useStyles = makeStyles({
   root: {
-    color: "#3880ff",
+    color: '#3880ff',
     height: 2,
-    padding: "15px 0",
+    padding: '15px 0',
   },
   thumb: {
     height: 28,
     width: 28,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     // boxShadow: iOSBoxShadow,
     marginTop: -14,
     marginLeft: -14,
-    "&:focus, &:hover, &$active": {
+    '&:focus, &:hover, &$active': {
       boxShadow:
-        "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
+        '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
       // Reset on touch devices, it doesn't add specificity
       // "@media (hover: none)": {
       //   boxShadow: iOSBoxShadow,
@@ -27,7 +26,7 @@ const useStyles = makeStyles({
   },
   active: {},
   markLabel: {
-    marginTop: 15,
+    marginTop: 33,
   },
   track: {
     height: 2,
@@ -35,66 +34,110 @@ const useStyles = makeStyles({
   rail: {
     height: 2,
     opacity: 0.5,
-    backgroundColor: "#bfbfbf",
+    backgroundColor: '#bfbfbf',
   },
   mark: {
-    backgroundColor: "#bfbfbf",
+    backgroundColor: '#bfbfbf',
     height: 8,
     width: 1,
-    marginTop: 10,
+    marginTop: 28,
   },
 });
 
 const referrals = [
   {
     value: 4,
-    label: "4",
+    label: '4',
   },
   {
     value: 10,
-    label: "10",
+    label: '10',
   },
   {
     value: 20,
-    label: "20",
+    label: '20',
   },
   {
     value: 30,
-    label: "30",
+    label: '30',
   },
   {
     value: 40,
-    label: "40",
+    label: '40',
   },
   {
     value: 50,
-    label: "50",
+    label: '50',
   },
   {
     value: 60,
-    label: "60",
+    label: '60',
   },
   {
     value: 70,
-    label: "70",
+    label: '70',
   },
   {
     value: 80,
-    label: "80",
+    label: '80',
   },
   {
     value: 90,
-    label: "90",
+    label: '90',
   },
   {
     value: 100,
-    label: "100",
+    label: '100',
   },
 ];
 
-function valuetext(value) {
-  console.log(value);
-  return `${value}`;
+const RangeSlider = withStyles({
+  root: {
+    color: '#AF00CC',
+    height: 120,
+  },
+  thumb: {
+    width: 40,
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    border: '1px solid #AF00CC',
+    marginTop: -17,
+    marginLeft: -18,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
+    '& .bar': {
+      // display: inline-block !important;
+      height: 15,
+      width: 1,
+      backgroundColor: '#AF00CC',
+      marginLeft: 2,
+      marginRight: 2,
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+function RangeSliderThumbComponent(props) {
+  return (
+    <span {...props}>
+      <span className="bar" />
+      <span className="bar" />
+      <span className="bar" />
+    </span>
+  );
 }
 
 function valueLabelFormat(value) {
@@ -107,10 +150,8 @@ export default function SliderReferral({ valueChange }) {
 
   return (
     <div className={classes.root}>
-      <Slider
-        style={{
-          color: "#b267cc",
-        }}
+      <RangeSlider
+        ThumbComponent={RangeSliderThumbComponent}
         defaultValue={6}
         valueLabelFormat={valueLabelFormat}
         onChange={(ev, value) => valueChange(value)}
